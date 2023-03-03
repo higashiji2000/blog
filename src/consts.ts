d78ed79ca2584097c303402213b9f8b7a17fdb46
higashiji <higashiji.dev@gmail.com>
2023-03-04 00:07:11 +0900

    サイト名変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "bunibuni.dev";
+export const SITE_TITLE = "bunibuni";
 export const SITE_DESCRIPTION = "技術メモ・雑記";
 export const HTML_ID = "root";
 export const LIGHT_THEME = "lofi";
